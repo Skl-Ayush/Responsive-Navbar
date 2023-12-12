295c3f6bbdca351559d47a05f2853ae7d5635a22
Ayush Khanal <135571344+Skl-Ayush@users.noreply.github.com>
2023-12-12 03:48:33 -0800

    Update script.js

--- Responsive Navbar/script.js
+++ Responsive Navbar/script.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const colorCheckbox = document.getElementById('check');
-    const colorContainer = document.getElementsByTagName('nav')[0];
-
-    colorCheckbox.addEventListener('change', function () {
-        if (colorCheckbox.checked) {
+    const Checkbox = document.getElementById('check');
+    
+    Checkbox.addEventListener('change', function () {
+        if (Checkbox.checked) {
                     document.getElementsByTagName('ul')[0].style.left="0%";
                 } else {
                     document.getElementsByTagName('ul')[0].style.left="-100%";
